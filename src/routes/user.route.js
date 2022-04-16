be865bcba30e82c870d5e6669d1eeb43aca384bc
Sergio Peña <sergiopena@MacBook-Air-de-Sergio.local>
2022-04-16 15:19:16 -0500

    Adds checkout process view

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getUser, getUserLogin, getPurchase, getCart } = require("../controller/user.controller");
+const { getUser, getUserLogin, getPurchase, getCart, getProcess } = require("../controller/user.controller");
 
 
 router.get("/", getUser);
@@ -9,6 +9,8 @@ router.get("/", getUser);
 router.get("/login", getUserLogin);
 
 router.get("/my_purchases", getPurchase)
+
+router.get("/checkout", getProcess)
 
 router.get("/cart", getCart)
 
